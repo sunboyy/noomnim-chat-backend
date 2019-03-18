01a4c656d17788f2f93fe08af37047fc6870ee53
Natchaporn Ponkitsaran <natchaporn.pon@gmail.com>
2019-03-18 16:08:51 +0700

    Fix with comment

--- src/models/member.js
+++ src/models/member.js
@@ -1,34 +1,46 @@
-import { getConnection } from './database'
+import { getConnection } from './database';
 
 export function getMembership(clientId) {
-    return new Promise(async (resolve, reject) => {
-        const conn = await getConnection()
-        conn.query('SELECT `group`.`id`, `group`.`name` FROM `member` NATURAL JOIN `group` WHERE `member`.`client_id` = ?', [clientId], (err, rows) => {
-            if (err) reject('Cannot get membership')
-            else resolve(rows)
-            conn.release()
-        })
-    })
+  return new Promise(async (resolve, reject) => {
+    const conn = await getConnection();
+    conn.query(
+      'SELECT `group`.`id`, `group`.`name` FROM `member` NATURAL JOIN `group` WHERE `member`.`client_id` = ?',
+      [clientId],
+      (err, rows) => {
+        if (err) reject('Cannot get membership');
+        else resolve(rows);
+        conn.release();
+      }
+    );
+  });
 }
 
 export function checkMembership(clientId, groupId) {
-    return new Promise(async (resolve, reject) => {
-        const conn = await getConnection()
-        conn.query('SELECT * FROM `member` WHERE `client_id` = ? AND `group_id` = ?', [clientId, groupId], (err, rows) => {
-            if (err) reject('Cannot check membership')
-            else resolve(rows.length > 0)
-            conn.release()
-        })
-    })
+  return new Promise(async (resolve, reject) => {
+    const conn = await getConnection();
+    conn.query(
+      'SELECT * FROM `member` WHERE `client_id` = ? AND `group_id` = ?',
+      [clientId, groupId],
+      (err, rows) => {
+        if (err) reject('Cannot check membership');
+        else resolve(rows.length > 0);
+        conn.release();
+      }
+    );
+  });
 }
 
 export function addMembership(clientId, groupId) {
-    return new Promise(async (resolve, reject) => {
-        const conn = await getConnection()
-        conn.query('INSERT INTO `member` (`client_id`,`group_id`,`last_msg_id` VALUES (?,?,NULL)', [clientId, groupId], (err, res) => {
-            if(err) reject('Cannot join group')
-            else resolve({ id: res.insertId, name })
-        })
-        conn.release()
-    })
+  return new Promise(async (resolve, reject) => {
+    const conn = await getConnection();
+    conn.query(
+      'INSERT INTO `member` (`client_id`,`group_id`,`last_msg_id` VALUES (?,?,NULL)',
+      [clientId, groupId],
+      (err, res) => {
+        if (err) reject('Cannot join group');
+        else resolve({ id: res.insertId, clientId, groupId });
+        conn.release();
+      }
+    );
+  });
 }
--- src/socket-handler.js
+++ src/socket-handler.js
@@ -1,50 +1,51 @@
-import socketIO from 'socket.io'
-import { getClient, insertClient } from './models/client'
-import { getMembership, addMembership } from './models/member'
+import socketIO from 'socket.io';
+import { getClient, insertClient } from './models/client';
+import { getMembership, addMembership } from './models/member';
 
-let io
+let io;
 
 export function initSocket(http) {
-    io = socketIO(http)
-    io.on('connection', socket => {
-        socket.emit('greet', 'Hello, a new client!')
-        console.log(socket.id + ': Connected')
+  io = socketIO(http);
+  io.on('connection', socket => {
+    socket.emit('greet', 'Hello, a new client!');
+    console.log(socket.id + ': Connected');
 
-        socket.on('greet', msg => {
-            console.log('' + socket.id + ' [greet]: ' + msg + '')
-        })
+    socket.on('greet', msg => {
+      console.log('' + socket.id + ' [greet]: ' + msg + '');
+    });
 
-        socket.on('create-client', async (msg) => {
-            try {
-                let client = await getClient(msg)
-                if (!client) {
-                    client = await insertClient(msg)
-                }
-                const groups = await getMembership(client.id)
-                groups.forEach(group => socket.join('group/' + group.id))
-                socket.emit('create-client', { data: { client, groups } })
-            } catch (e) {
-                socket.emit('create-client', { error: e })
-            }
-        })
+    socket.on('create-client', async msg => {
+      try {
+        let client = await getClient(msg);
+        if (!client) {
+          client = await insertClient(msg);
+        }
+        const groups = await getMembership(client.id);
+        groups.forEach(group => socket.join('group/' + group.id));
+        socket.emit('create-client', { data: { client, groups } });
+      } catch (e) {
+        socket.emit('create-client', { error: e });
+      }
+    });
 
-        socket.on('join-group', async (name, groupId) => {
-            try {
-                const client = await getClient(name)
-                if (!client) {
-                    socket.emit('create-client', { error: 'Create client first!' })
-                }
-                if (!checkMembership(clientId, groupId)) {
-                    await addMembership(clientId, groupId)
-                }
-                socket.join('group/' + groupId)
-            } catch (e) {
-                console.error(e)
-            }
-        })
-    })
+    socket.on('join-group', async (name, groupId) => {
+      try {
+        const client = await getClient(name);
+        if (!client) {
+          socket.emit('join-group', { error: 'Create client first!' });
+        }
+        if (!checkMembership(clientId, groupId)) {
+          await addMembership(clientId, groupId);
+        }
+        socket.join('group/' + groupId);
+      } catch (e) {
+        console.error(e);
+        socket.emit('join-group', { error: e });
+      }
+    });
+  });
 }
 
 export function pushToGroup(groupId, message) {
-    io.to('group/' + groupId).emit('message', { data: message })
+  io.to('group/' + groupId).emit('message', { data: message });
 }
